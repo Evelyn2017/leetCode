3d79163d61bddff8d581f211194ead14483a764b
Evelyn2017 <evelinx@foxmail.com>
2018-03-02 15:59:31 +0800

    单链表反转

--- src/LinkedList/Node.java
+++ src/LinkedList/Node.java
@@ -50,20 +50,60 @@ class NodeList{
 
     /**
      * 在头结点后插入结点
+     * 当单链表为空进行插入时，头尾指针均指向插入的结点
+     * 当单链表不为空插入时，头指针不变，先定位尾指针，再在尾指针后插入
      */
     public void insertBack(int value){
         Node aNode = new Node(value);
-        Node cur = head;
+        Node tail = head;
         if(head == null){
             aNode.next = head;
             head = aNode;
         }
         else{
-            while(cur.next != null)
-                cur = cur.next;
-            cur.next = aNode;
+            //寻找尾结点
+            while(tail.next != null)
+                tail = tail.next;
+            //在尾结点插入新结点
+            tail.next = aNode;
         }
 
+    }
+
+    /**
+     * 单链表反转
+     */
+
+    public NodeList reverse(NodeList list){
+
+        NodeList rev = new NodeList();
+        while(list.head != null){
+            rev.insert(list.head.value);
+            head = head.next;
+        }
+        return rev;
+    }
+
+    public Node reverseNode(Node h){
+        Node rev = new Node();
+        while(h.next != null){
+            rev.next = h;
+            h = rev;
+        }
+        return rev;
+    }
+
+    public Node reverseCur(Node cur){
+        Node pre = null;
+        Node nex = null;
+
+        while(cur != null){
+            nex = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = nex;
+        }
+        return pre;
     }
 
     /**
@@ -115,6 +155,18 @@ class testLinkedList{
         listb.insertBack(4);
 
         listb.display();
+
+        NodeList rev = listb.reverse(listb);
+        rev.display();
+
+        System.out.println();
+        Node a = list.reverseCur(list.head);
+        while(a != null){
+            System.out.print(a.value + " ");
+            a = a.next;
+        }
+
+
 
 //        System.out.println("delete");
 //        list.delete();
