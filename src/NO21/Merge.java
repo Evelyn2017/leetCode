a61bb15a96500fddb5c15e299e2e493f4c9bbffd
Evelyn2017 <evelinx@foxmail.com>
2018-03-04 14:44:35 +0800

    递归方法合并单链表

--- src/NO21/Merge.java
+++ src/NO21/Merge.java
@@ -32,6 +32,18 @@ public class Merge {
         return res;
     }
 
+    public Node merge(Node l1, Node l2){
+        if(l1 == null) return l2;
+        if(l2 == null) return l1;
+        if(l1.value < l2.value){
+            l1.next = merge(l1.next, l2);
+            return l1;
+        } else{
+            l2.next = merge(l1, l2.next);
+            return l2;
+        }
+    }
+
     public static void main(String[] args){
 
     }
