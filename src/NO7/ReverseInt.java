754610e09d178c73eb6143a8f45c5a47d647cde8
Evelyn2017 <evelinx@foxmail.com>
2019-03-07 11:40:19 +0800

    fixed a bug

--- src/NO7/ReverseInt.java
+++ src/NO7/ReverseInt.java
@@ -3,11 +3,11 @@ package NO7;
 public class ReverseInt {
     public static int reverseInteger(int x) {
         int result = 0;
-        //int newRes;
+        int newRes;
 
         while (x != 0) {
             int tail = x % 10;
-            int newRes = result * 10 + tail;
+            newRes = result * 10 + tail;
 
             if ((newRes - tail) / 10 != result)
                 return Integer.MIN_VALUE;
