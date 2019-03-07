05cf79123b0be70d22b2e74777cfa110255212e7
Evelyn2017 <evelinx@foxmail.com>
2019-03-07 11:06:49 +0800

    No 4 Median of two sorted array.

---
+++ src/NO9/LongestPanlidromic.java
@@ -0,0 +1,29 @@
+package NO9;
+
+public class LongestPanlidromic {
+    private int start;
+    private int maxLen;
+
+    public String getLongestsub(String s) {
+        if (s.length() < 2)
+            return s;
+
+        for (int i = 0; i < s.length(); i++) {
+            expand(s, i, i); //assume the result is odd
+            expand(s, i, i + 1); //assume the result is even
+        }
+
+        return s.substring(start, start + maxLen);
+    }
+
+    public void expand(String s, int j, int k) {
+        while (j >= 0 && k < s.length() && s.charAt(j) == s.charAt(k)) {
+            if (maxLen < k - j + 1) {
+                maxLen = k - j + 1;
+                start = j;
+            }
+            j--;
+            k++;
+        }
+    }
+}
