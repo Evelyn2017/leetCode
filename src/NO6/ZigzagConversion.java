fc57b0121cea520de16b9f0932be1b047ac85208
Evelyn2017 <evelinx@foxmail.com>
2019-03-08 22:31:02 +0800

    NO6 zigzag conversion

---
+++ src/NO6/ZigzagConversion.java
@@ -0,0 +1,45 @@
+package NO6;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ZigzagConversion {
+    /**
+     *
+     * @param s abcdefghin
+     * @param numRow 3
+     * @return aeibdfhncg
+     *         a   e   i
+     *         b d f h n
+     *         c   g
+     */
+    public static String conversion(String s, int numRow) {
+        if (s.length() == 1)
+            return s;
+
+        int currRow = 0;
+        boolean goingDown = false;
+
+        List<StringBuilder> rows = new ArrayList<>(); //store row
+        for(int i=0;i<Math.min(numRow, s.length()); i++)
+            rows.add(new StringBuilder());
+
+        for (char c : s.toCharArray()) {
+            rows.get(currRow).append(c);
+            if (currRow == 0 || currRow == numRow - 1)
+                goingDown = !goingDown;
+            currRow += goingDown ? 1 : -1;
+        }
+
+        StringBuilder result = new StringBuilder();
+        for(StringBuilder row : rows)
+            result.append(row);
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        String s = "abcdefghin";
+        System.out.println(conversion(s, 3));
+    }
+}
