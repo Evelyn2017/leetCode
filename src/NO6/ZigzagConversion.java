d258b195a18a10fd064350b4295a790c68b39f44
Evelyn2017 <evelinx@foxmail.com>
2019-03-08 22:33:47 +0800

    private static

--- src/NO6/ZigzagConversion.java
+++ src/NO6/ZigzagConversion.java
@@ -5,13 +5,12 @@ import java.util.List;
 
 public class ZigzagConversion {
     /**
-     *
-     * @param s abcdefghin
+     * @param s      abcdefghin
      * @param numRow 3
      * @return aeibdfhncg
-     *         a   e   i
-     *         b d f h n
-     *         c   g
+     * a   e   i
+     * b d f h n
+     * c   g
      */
     private static String conversion(String s, int numRow) {
         if (s.length() == 1)
@@ -21,18 +20,18 @@ public class ZigzagConversion {
         boolean goingDown = false;
 
         List<StringBuilder> rows = new ArrayList<>(); //store row
-        for(int i=0;i<Math.min(numRow, s.length()); i++)
+        for (int i = 0; i < Math.min(numRow, s.length()); i++)
             rows.add(new StringBuilder());
 
         for (char c : s.toCharArray()) {
-            rows.get(currRow).append(c); //
-            if (currRow == 0 || currRow == numRow - 1)
+            rows.get(currRow).append(c); //遍历字符串，把当前字符分配给第i个row
+            if (currRow == 0 || currRow == numRow - 1) //判断是否转换方向
                 goingDown = !goingDown;
-            currRow += goingDown ? 1 : -1;
+            currRow += goingDown ? 1 : -1; //向下走currRow - 1； 向上currRow + 1
         }
 
         StringBuilder result = new StringBuilder();
-        for(StringBuilder row : rows)
+        for (StringBuilder row : rows)
             result.append(row);
 
         return result.toString();
